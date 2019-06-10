01aabe9db4dd9abbbc2ede018a9e3624a29b1a90
jpetucci <jpetucci@du.edu>
2019-06-10 15:59:58 -0400

    Update latest.lua

--- latest.lua
+++ latest.lua
@@ -12,7 +12,7 @@ whatis('URL: https://www.tensorflow.org')
 whatis('singularity pull shub://jpetucci/tensorflow_icsaci:rec')
 unsetenv("LD_PRELOAD")
 local python = [==[
-/usr/bin/singularity run /path/to/singularity/image "$@";
+/usr/bin/singularity exec /path/to/singularity/image python "$@";
 ]==]
 
 set_shell_function("python",python,python)
